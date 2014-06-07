67fc3176b375d04c539a1c5cb51b696cd0a8ec58
rainasmoon <rainasmoon@126.com>
2014-06-07 13:58:19 +0800

    select

--- online-application/src/main/java/com/hawk/application/web/DictionaryTypeFormatter.java
+++ online-application/src/main/java/com/hawk/application/web/DictionaryTypeFormatter.java
@@ -5,18 +5,23 @@ import java.util.Arrays;
 import java.util.Collection;
 import java.util.Locale;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.format.Formatter;
 
 import com.hawk.application.model.Dictionary;
 import com.hawk.application.model.PlatformType;
 
 public class DictionaryTypeFormatter implements Formatter<Dictionary> {
+	Logger LOGGER = LoggerFactory.getLogger(DictionaryTypeFormatter.class);
 
 	public String print(Dictionary dictionary, Locale locale) {
 		return dictionary.getDictionaryValue();
 	}
 
 	public Dictionary parse(String text, Locale locale) throws ParseException {
+		LOGGER.debug("the format text is:" + text);
+		
 		return new Dictionary();
 	}
 }
