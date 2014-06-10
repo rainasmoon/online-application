9289046bc92cf057ed78b4c13c5fd1fdfc28deec
rainasmoon <rainasmoon@126.com>
2014-06-10 11:49:35 +0800

    sdk download sys location

--- online-application/src/main/java/com/hawk/application/web/SdkDownloadController.java
+++ online-application/src/main/java/com/hawk/application/web/SdkDownloadController.java
@@ -9,6 +9,9 @@ import javax.servlet.http.HttpServletResponse;
 import org.apache.commons.io.IOUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 import org.springframework.dao.DataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
@@ -19,10 +22,16 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @Controller
+@PropertySource("classpath:/spring/data-access.properties")
 public class SdkDownloadController {
 
 	private static final Logger LOGGER = LoggerFactory
 			.getLogger(SdkDownloadController.class);
+
+	@Autowired
+	Environment env;
+
+	private static String SYS_SDK_PATH;
 
 	@ResponseStatus(value = HttpStatus.NOT_FOUND, reason = "File not found")
 	@ExceptionHandler(SdkNotFoundException.class)
@@ -37,8 +46,13 @@ public class SdkDownloadController {
 			SdkNotFoundException {
 		FileInputStream fileInputStream = null;
 		try {
-			File file = new File("test_download_file.zip");
-			file.createNewFile();
+			SYS_SDK_PATH = env.getProperty("sdk.download.location");
+
+			LOGGER.debug("!!!!!the SYS_SDK_PATH is " + SYS_SDK_PATH);
+
+			File file = new File(SYS_SDK_PATH + File.separator
+					+ "test_download_file.zip");
+
 			fileInputStream = new FileInputStream(file);
 
 			response.setHeader("Content-Disposition", "attachment; filename="
