5177aeab70d0049f3b2965ef0b164c151973beb5
rainasmoon <rainasmoon@126.com>
2014-06-13 19:04:51 +0800

    set id to null so that when apply check it will create a new one.

--- online-application/src/main/java/com/hawk/application/web/FinanceController.java
+++ online-application/src/main/java/com/hawk/application/web/FinanceController.java
@@ -14,48 +14,47 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
-import com.hawk.application.model.Application;
+import com.hawk.application.model.Bonus;
+import com.hawk.application.model.Check;
 import com.hawk.application.model.User;
-import com.hawk.application.service.ApplicationService;
+import com.hawk.application.service.FinanceService;
 import com.hawk.application.service.UserService;
-
-import com.hawk.application.model.*;
-import com.hawk.application.service.*;
 
 @Controller
 @SessionAttributes(types = Check.class)
 public class FinanceController {
-		
+
 	private final FinanceService financeService;
 	@Autowired
 	private UserService userService;
-    @Autowired
-    private Mapper dozerBeanMapper;
-	
+	@Autowired
+	private Mapper dozerBeanMapper;
+
 	@Autowired
 	public FinanceController(FinanceService financeService) {
 		this.financeService = financeService;
 	}
 
 	@RequestMapping(value = "/checks/new", method = RequestMethod.GET)
-	public String initCreationForm(HttpSession session, Map<String, Object> model) {
-		
+	public String initCreationForm(HttpSession session,
+			Map<String, Object> model) {
+
 		String userEmail = (String) session.getAttribute("userEmail");
 		User user = userService.findUserByEmail(userEmail);
-		
+
 		Check check = dozerBeanMapper.map(user, Check.class);
+		check.setId(null);
 		model.put("check", check);
 
 		return "finance/applyCheck";
 	}
 
 	@RequestMapping(value = "/checks/new", method = RequestMethod.POST)
-	public String processCreationForm(@Valid Check check,
-			BindingResult result) {
+	public String processCreationForm(@Valid Check check, BindingResult result) {
 		if (result.hasErrors()) {
 			return "finance/applyCheck";
 		} else {
-			this.financeService.saveCheck(check);			
+			this.financeService.saveCheck(check);
 			return "redirect:/checks";
 		}
 	}
@@ -63,19 +62,17 @@ public class FinanceController {
 	@RequestMapping(value = "/checks", method = RequestMethod.GET)
 	public String processFindAllChecks(Map<String, Object> model) {
 
-		List<Check> results = this.financeService
-				.findAllChecks();
+		List<Check> results = this.financeService.findAllChecks();
 
 		model.put("selections", results);
 		return "finance/listCheck";
 
 	}
-	
+
 	@RequestMapping(value = "/bonus", method = RequestMethod.GET)
 	public String processFindAllBonus(Map<String, Object> model) {
 
-		List<Bonus> results = this.financeService
-				.findAllBonus();
+		List<Bonus> results = this.financeService.findAllBonus();
 
 		model.put("selections", results);
 		return "finance/listBonus";
