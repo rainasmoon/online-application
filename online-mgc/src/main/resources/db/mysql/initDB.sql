175e278280a05f72e46c4574ca0b98e8f2f60d7c
rainasmoon <rainasmoon@126.com>
2014-06-20 15:46:42 +0800

    init mysql db

--- online-mgc/src/main/resources/db/mysql/initDB.sql
+++ online-mgc/src/main/resources/db/mysql/initDB.sql
@@ -7,41 +7,29 @@ USE onlinemgc;
 
 CREATE TABLE IF NOT EXISTS users (
   id INT(4) UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  email VARCHAR(100),  
+  user_name VARCHAR(100),
   password  VARCHAR(100),
-  qq  VARCHAR(20),
-  mobile  VARCHAR(20),
-  contact_type VARCHAR(20),
-  contact_name VARCHAR(100),
-  contact_id_number VARCHAR(20),
-  bank_name VARCHAR(100),
-  province_id INT(4),
-  city_id INT(4),
-  branch_name VARCHAR(200),
-  account_number VARCHAR(50),
-  id_card_front_path VARCHAR(100),
-  id_card_back_path VARCHAR(100),
+  user_role VARCHAR(50),
   created_date  TIMESTAMP,
   updated_date  TIMESTAMP,
   created_by INT(4),
   updated_by INT(4),  
-  INDEX(email)
+  INDEX(user_name)
 ) engine=InnoDB CHARSET=utf8;
 
 
 CREATE TABLE IF NOT EXISTS packages (
   id INT(4) UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  dianjoy_app_id VARCHAR(40),
-  application_name  VARCHAR(100),
-  application_platform  VARCHAR(10),
-  application_package_name  VARCHAR(500),
-  status  VARCHAR(50),
+  package_name VARCHAR(100),
+  production_name VARCHAR(100),
+  installations INT(4) UNSIGNED,
+  activations INT(4) UNSIGNED,
   created_date  TIMESTAMP,
   updated_date  TIMESTAMP,
   created_by INT(4) UNSIGNED,
   updated_by INT(4) UNSIGNED,
-  INDEX(created_by),
-  FOREIGN KEY (created_by) REFERENCES users(id),
-  FOREIGN KEY (updated_by) REFERENCES users(id)
-) engine=InnoDB CHARSET=utf8;
+  INDEX(package_name),
+  INDEX(production_name),
+  INDEX(created_date)
+ ) engine=InnoDB CHARSET=utf8;
 
---
+++ online-mgc/src/main/resources/db/mysql/populateDB.sql
@@ -0,0 +1,4 @@
+INSERT IGNORE INTO users VALUES (1, 'manager', 'test', 'manager', '2013-09-07','2010-09-07', 1, 1);
+INSERT IGNORE INTO users VALUES (2, 'user', 'test', 'user', '2013-09-07','2010-09-07', 1, 1);
+
+INSERT IGNORE INTO packages VALUES (1, 'djz_21.apk', '大家赚', 1000, 2000, '2013-09-07','2010-09-07', 1, 1);
