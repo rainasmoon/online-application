f703aab906783c9662283de7efda9bc162c85477
glen <rainasmoon@126.com>
2016-01-13 16:02:32 +0800

    init

---
+++ mc-war-test-me/src/test/java/com/wanlianjin/mc/war/RemoteUserServiceTest.java
@@ -0,0 +1,287 @@
+package com.wanlianjin.mc.war;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+
+import java.util.Date;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.jdbc.SqlConfig;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.kahadb.utility.exception.ApplicationException;
+import com.kahadb.utility.exception.ParameterException;
+import com.wanlianjin.mc.user.domian.UserLogin;
+import com.wanlianjin.mc.user.dto.UserDto;
+import com.wanlianjin.mc.user.enums.ChannelEnum;
+import com.wanlianjin.mc.user.service.UserService;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = { "classpath*:remoting-client.xml", "classpath*:app-db-test.xml" })
+@Rollback(true)
+@Sql(value = { "classpath:insert-test-data.sql" }, config = @SqlConfig(encoding = "utf-8", separator = ";", commentPrefix = "--", dataSource = "dataSource", transactionManager = "transactionManager") )
+
+public class RemoteUserServiceTest {
+
+	private static Logger log = LoggerFactory.getLogger(RemoteUserServiceTest.class);
+
+	private ApplicationContext context = new ClassPathXmlApplicationContext("/remoting-client.xml");
+
+	private UserService userService = (UserService) context.getBean("userService");
+
+	@Before
+	public void setup() {
+	}
+
+	@After
+	public void clear() {
+	}
+
+	@Test
+	public void testAdd() {
+		UserDto userDto = new UserDto();
+		userDto.setUserId("11112");
+		userDto.setUserAccount("test");
+		userDto.setPassword("111");
+		userDto.setMobilePhone("13690000123");
+		userDto.setStatus(1);
+		userDto.setEnChannel(ChannelEnum.WANLIAN);
+		userDto.setChannelUserId("testChannelUserId");
+		userDto.setCreateDate(new Date());
+		userService.add(userDto);
+		System.out.println(1);
+	}
+
+	@Test
+	public void testAddWithRecordLoginInfo() {
+		UserDto userDto = new UserDto();
+		userDto.setUserId("11112");
+		userDto.setUserAccount("test");
+		userDto.setPassword("111");
+		userDto.setMobilePhone("13690000123");
+		userDto.setStatus(1);
+		userDto.setEnChannel(ChannelEnum.WANLIAN);
+		userDto.setChannelUserId("testChannelUserId");
+		userDto.setCreateDate(new Date());
+		String userId = userService.add(userDto, "1.1.0.1", "testData");
+		assertNotNull(userId);
+
+		UserDto r = userService.getByUserId(userId);
+		assertNotNull(r);
+		assertNotNull(r.getUserLogin());
+		assertEquals("testData", r.getUserLogin().getLastLoginAppnum());
+	}
+
+	@Test
+	public void testAddThirdUser() {
+		UserDto userDto = new UserDto();
+		userDto.setUserId("11112");
+		userDto.setUserAccount("testThirdAccount");
+		userDto.setPassword("111");
+		userDto.setMobilePhone("1369");
+		userDto.setStatus(1);
+		userDto.setEnChannel(ChannelEnum.WEIXIN);
+		userDto.setChannelUserId("testChannelUserId");
+		userDto.setCreateDate(new Date());
+		String userId = userService.addThirdUser(userDto);
+		log.info("add third user success.");
+		log.info(userId);
+		assertNotNull(userId);
+
+		UserDto r = userService.getByUserId(userId);
+		assertNotNull(r);
+
+	}
+
+	@Test(expected = ApplicationException.class)
+	public void testAddThirdUserWithExistException() {
+		UserDto userDto = new UserDto();
+		userDto.setUserId("11112");
+		userDto.setUserAccount("testThirdAccount");
+		userDto.setPassword("111");
+		userDto.setMobilePhone("1369");
+		userDto.setStatus(1);
+		userDto.setEnChannel(ChannelEnum.WEIXIN);
+		userDto.setChannelUserId("testChannelUserId");
+		userDto.setCreateDate(new Date());
+		userService.addThirdUser(userDto);
+		userService.addThirdUser(userDto);
+		log.info("add third user success.");
+	}
+
+	@Test
+	public void login() {
+		UserDto userDto = userService.login("testGlen", "111111");
+		assertNotNull(userDto);
+		assertEquals("testGlen", userDto.getUserAccount());
+	}
+
+	@Test
+	public void existAccountName() {
+		Boolean flag = userService.existAccountName("test1");
+		System.out.println(flag);
+	}
+
+	@Test
+	public void existMobile() {
+		Boolean flag = userService.existMobile("13456781234");
+		assertTrue(flag);
+	}
+
+	@Test
+	public void shouldLoginThirdChannelUser() {
+		UserDto userDto = new UserDto();
+		userDto.setEnChannel(ChannelEnum.CICPROPERTY);
+		userDto.setChannelUserId("test3rdUserid_1");
+
+		UserLogin userLogin = new UserLogin();
+		userLogin.setLastLoginAppnum("test3rdloginappclient");
+		userDto.setUserLogin(userLogin);
+
+		String r = userService.login3rdUser(userDto);
+		assertNotNull(r);
+
+		r = userService.login3rdUser(userDto);
+		assertNotNull(r);
+
+		UserDto result = userService.getByUserId(r);
+		assertNotNull(result);
+		assertNotNull(result.getUserLogin());
+		assertEquals("test3rdloginappclient", result.getUserLogin().getLastLoginAppnum());
+
+	}
+
+	@Test
+	public void shouldLoginThirdChannelUser_v2() {
+		UserDto userDto = new UserDto();
+		userDto.setEnChannel(ChannelEnum.CICPROPERTY);
+		userDto.setChannelUserId("test3rdUserid_1");
+
+		String r = userService.login3rdUser(userDto, "1.1.2.4", "test3rdloginappclient");
+		assertNotNull(r);
+
+		r = userService.login3rdUser(userDto);
+		assertNotNull(r);
+
+		UserDto result = userService.getByUserId(r);
+		assertNotNull(result);
+		assertNotNull(result.getUserLogin());
+		assertEquals("test3rdloginappclient", result.getUserLogin().getLastLoginAppnum());
+
+	}
+
+	@Test(expected = ParameterException.class)
+	public void existMobileWithPhoneException() {
+		Boolean flag = userService.existMobile("13456");
+		assertTrue(flag);
+	}
+
+	@Test
+	public void testUpdate() {
+		UserDto userDto = new UserDto();
+		userDto.setUserId("11112");
+		userDto.setStatus(1);
+		// userDto.setEnChannel(ChannelEnum.WANLIAN);
+		userDto.setCreateDate(new Date());
+		userService.update(userDto);
+		System.out.println(1);
+	}
+
+	@Test
+	public void testCheckPassword() {
+		boolean flag = userService.checkPassword("111112", "111");
+		System.out.println(flag);
+	}
+
+	@Test
+	public void testUpdatePassWord() {
+		boolean flag = userService.updatePassword("11112", "111111", "222222");
+		assertTrue(flag);
+
+		flag = userService.updatePassword("11112", "111111", "222222");
+		assertFalse(flag);
+
+		UserDto user = userService.login("testGlen", "111111", "testIp", "testLoginapp");
+		assertNull(user);
+
+		user = userService.login("testGlen", "222222", "testIp", "testLoginapp");
+		assertNotNull(user);
+		assertEquals("testGlen", user.getUserAccount());
+	}
+
+	@Test
+	public void testResetPassWord() {
+		boolean flag = userService.updatePassWord("11112", "222222");
+		assertTrue(flag);
+
+		UserDto user = userService.login("testGlen", "111111", "testIp", "testLoginapp");
+		assertNull(user);
+
+		user = userService.login("testGlen", "222222", "testIp", "testLoginapp");
+		assertNotNull(user);
+		assertEquals("testGlen", user.getUserAccount());
+	}
+
+	@Test
+	public void testUpdateMobile() {
+		boolean flag = userService.updateMobile("11112", "13456753567");
+		System.out.println(flag);
+	}
+
+	@Test
+	public void testGetByLoginName() {
+		UserDto loginUser = userService.getByLoginName("testGlen");
+		assertNotNull(loginUser);
+		assertEquals("testGlen", loginUser.getUserAccount());
+	}
+
+	@Test
+	public void testGetByChannelUserId() {
+		UserDto userDto = userService.getByChannelUserId(ChannelEnum.WEIXIN, "testChannelUserId");
+		assertNotNull(userDto);
+		assertEquals("testChannelUserId", userDto.getChannelUserId());
+	}
+
+	@Test
+	public void testGetByChannelUserIdIsNull() {
+		UserDto userDto = userService.getByChannelUserId(ChannelEnum.WEIXIN, "testNotExistUserId");
+		assertNull(userDto);
+
+	}
+
+	@Test
+	public void testBindChannelUser() {
+		userService.bindChannelUser(ChannelEnum.WEIXIN, "11112", "testWexinGlen");
+
+		UserDto userDto = userService.getByChannelUserId(ChannelEnum.WEIXIN, "testWexinGlen");
+		assertNotNull(userDto);
+		assertEquals("testChannelUserId", userDto.getChannelUserId());
+	}
+
+	@Test
+	public void login_v2() {
+		UserDto userDto = userService.login("testGlen", "111111", "127.0.0.1", "rewfr345red");
+
+		assertNotNull(userDto);
+		assertEquals("testGlen", userDto.getUserAccount());
+	}
+
+	@Test
+	public void testLog() {
+		log.info("hi log...");
+	}
+
+}
---
+++ mc-war-test-me/src/test/resources/insert-test-data.sql
@@ -0,0 +1,19 @@
+delete from T_MC_USER_CHANNEL where user_id = '11112';
+delete from T_MC_USER where user_id = '11112' or mobile_phone = '13690000123' or user_account = 'testThirdAccount';
+delete from t_Mc_User_Login where user_id = '11112';
+
+--111111
+insert into T_MC_USER (USER_ID, USER_ACCOUNT, PASSWORD, EMAIL, MOBILE_PHONE, NICK_NAME, USER_NAME, IDENTIFY_TYPE, IDENTIFY_NUMBER, SEX, BIRTHDAY, HOME_PHONE, OFFICE_PHONE, CONTACT_ADDRESS, ZIP_CODE, USER_LOGO, USER_TYPE, REGISTER_TYPE, STATUS, CHANNEL_USER_ID, CHANNEL_USER_FLAG, CREATE_DATE, UPDATE_DATE, CHECK_STATUS, USER_LEVEL, FLAG)
+values ('11112', 'testGlen', '5eb72c92a549dd5a33011296e7921896', null, '13456781234', null, null, 0, null, 0, null, null, null, null, null, null, 0, 0, 1, 'testChannelUserId', '00', to_date('31-12-2015', 'dd-mm-yyyy'), to_date('31-12-2015', 'dd-mm-yyyy'), 0, null, null);
+
+insert into T_MC_USER_CHANNEL (USER_ID, CHANNEL_USER_FLAG, CHANNEL_USER_ID, LAST_LOGIN_DATE)
+values ('11112', '10', 'testChannelUserId', null);
+
+insert into t_Mc_User_Login (USER_ID, LAST_LOGIN_DATE, LAST_LOGIN_IP, LAST_LOGIN_APPNUM)
+values ('11112', to_date('08-01-2016 13:00:48', 'dd-mm-yyyy hh24:mi:ss'), '1.2.3.4', 'testData');
+
+commit;
+
+select * from T_MC_USER T where USER_ACCOUNT = 'testGlen' OR MOBILE_PHONE= 'testGlen';
+
+
