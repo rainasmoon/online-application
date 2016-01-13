76c8bb9e51d94764951882215ee9ea1d2eec2a33
glen <rainasmoon@126.com>
2016-01-13 16:01:31 +0800

    magrant data

--- local-deploy-3rdpart/autoupload.bat
+++ local-deploy-3rdpart/autoupload.bat
@@ -1,14 +1,22 @@
-cd D:/workspace/platform
+call cd D:/workspace/platform
+
 set JAVA_HOME=D:/jdk1.8.0_66
 set MAVEN_HOME=D:/apache-maven-3.0.3
 export PATH=.:/cygdrive/d/apache-maven-3.0.3/bin:`printenv PATH`
+
 cd D:/workspace/platform/util/trunk/project-parent
-call mvn clean install
+call D:/apache-maven-3.0.3/bin/mvn clean install
+pause
+
 cd D:/workspace/platform/util/trunk/utility
-call mvn clean install
+call D:/apache-maven-3.0.3/bin/mvn clean install
+pause
+
 set JAVA_HOME=D:/jdk1.6.0_10
 cd D:/workspace/platform/core/trunk/mc
-call mvn clean install
+call D:/apache-maven-3.0.3/bin/mvn clean install
+pause
+
 cd D:/workspace/platform/core/trunk/mc/mc-war/target
 scp ./mc-war-1.0.0.war weblogic@10.196.112.26:/weblogic/webapp/mc-war
 
--- local-deploy-3rdpart/sql/magrant.sql
+++ local-deploy-3rdpart/sql/magrant.sql
@@ -1,7 +1,13 @@
-
-
+delete from T_MC_USER;
+delete from T_MC_USER_LOGIN;
+-- the old table's lastdate & userid
+--  USER_TYPE(1), REGISTER_TYPE(1), UPDATE_DATE
 
 insert into T_MC_USER (USER_ID, USER_ACCOUNT, PASSWORD, EMAIL, MOBILE_PHONE, NICK_NAME, USER_NAME, IDENTIFY_TYPE, IDENTIFY_NUMBER, SEX, BIRTHDAY, HOME_PHONE, OFFICE_PHONE, CONTACT_ADDRESS, ZIP_CODE, USER_LOGO, USER_TYPE, REGISTER_TYPE, STATUS, CHANNEL_USER_ID, CHANNEL_USER_FLAG, CREATE_DATE, UPDATE_DATE, CHECK_STATUS, USER_LEVEL, FLAG)
-select USERID, useraccount, password, email, mobilephone, null, username, identifytype, identifynumber, sex, birthday, homephone, officephone, contactaddress, zipcode, null, null, null, status, channeluserid, channeluserflag, createdate, null, checkstatus, userlevel, flag from GE_USER_PERSONAL;
+select USERID, useraccount, CONCAT(SUBSTR(password, 11), SUBSTR(password, 1, 10)), email, mobilephone, null, username, identifytype, identifynumber, sex, birthday, homephone, officephone, contactaddress, zipcode, null, null, null, status, channeluserid, channeluserflag, createdate, null, checkstatus, userlevel, flag from GE_USER_PERSONAL;
 
--- the old table's lastdate & userid
+insert into t_Mc_User_Login (USER_ID, LAST_LOGIN_DATE, LAST_LOGIN_IP, LAST_LOGIN_APPNUM)
+select userid, lastdate, null, null from GE_USER_PERSONAL where lastdate is not null;
+
+commit;
+select * from T_MC_USER;
