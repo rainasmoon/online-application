9acfea769bda32463ad57374e64cc4d6722b9ea6
glen <rainasmoon@126.com>
2016-03-04 16:45:29 +0800

    work

---
+++ local-deploy-3rdpart/sql/delete-account.sql
@@ -0,0 +1,7 @@
+select * from T_MC_USER where mobile_phone = '15811015803';
+--delete from T_MC_USER where mobile_phone = '15811015803';
+select * from T_MC_INTEGRAL where user_id = '20000000013';
+select * from T_MC_INTEGRAL_DETAIL where user_id = '20000000013';
+select * from t_Mc_Wjb where user_id = '20000000013';
+select * from t_Mc_Wjb_Detail where user_id = '20000000013';
+commit;
