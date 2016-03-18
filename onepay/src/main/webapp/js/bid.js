ed08e73ce0b4f9d9d552d599a3dbb5a7e82566e5
glen <glen@hht-m>
2016-03-18 19:57:12 +0800

    add js validate for guess price input

--- onepay/src/main/webapp/js/bid.js
+++ onepay/src/main/webapp/js/bid.js
@@ -8,6 +8,15 @@ function addMoney(productId, amount) {
 }
 
 function guessMoney(productId) {
+	if ($("#guessPrice").val() == '') {
+		alert("请输入数字");
+		return;
+	}
+	var reg = new RegExp("^\\d+$"); 
+	if (!reg.test($("#guessPrice").val())) {
+		alert("请输入数字");
+		return;
+	}
 	$.post("restful/guess", {
 		productId : productId,
 		money : $("#guessPrice").val()
