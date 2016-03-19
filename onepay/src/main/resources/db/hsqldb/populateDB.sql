25aee2096e4f4d113b19fea40661a46a98bfb03b
glen <glen@hht-m>
2016-03-19 18:39:59 +0800

    add more order test datas

--- onepay/src/main/resources/db/hsqldb/populateDB.sql
+++ onepay/src/main/resources/db/hsqldb/populateDB.sql
@@ -1,2 +1,18 @@
-insert into users values(1, 'test@test.c', '1', '1', '测试账户', 0, 0, 0, 0, 0, 0, null);
+insert into users values(1, 'test@test.c', '1', '1', '测试账户', 0, 0, 0, 100, 0, 0, null);
+insert into users values(2, 'test@test.c', '2', '2', '测试账户2', 0, 0, 0, 0, 0, 0, null);
+
 insert into products values(1, '测试商品', 1, 1, 1, 1, 1, 1, null, 10, '测试的第一个商品', null);
+insert into products values(2, '测试商品2', 1, 1, 1, 1, 2, 1, null, 10, '测试的第2个商品', null);
+insert into products values(3, '测试商品3', 1, 1, 1, 1, 3, 1, null, 10, '测试的第3个商品', null);
+
+insert into orders values(1, 1, 2, 1, 100, 'test', '10086', 'earth', '100010', 'test', '10086', 'earth', '100010', 1, null);
+insert into orders values(2, 2, 1, 2, 100, 'test', '10086', 'earth', '100010', 'test', '10086', 'earth', '100010', 2, null);
+insert into orders values(3, 1, 2, 1, 100, 'test', '10086', 'earth', '100010', 'test', '10086', 'earth', '100010', 3, null);
+insert into orders values(4, 2, 1, 1, 100, 'test', '10086', 'earth', '100010', 'test', '10086', 'earth', '100010', 4, null);
+insert into orders values(5, 1, 2, 1, 100, 'test', '10086', 'earth', '100010', 'test', '10086', 'earth', '100010', 5, null);
+insert into orders values(6, 1, 2, 3, 100, 'test', '10086', 'earth', '100010', 'test', '10086', 'earth', '100010', 6, null);
+insert into orders values(7, 1, 2, 1, 100, 'test', '10086', 'earth', '100010', 'test', '10086', 'earth', '100010', 7, null);
+insert into orders values(8, 1, 2, 3, 100, 'test', '10086', 'earth', '100010', 'test', '10086', 'earth', '100010', 8, null);
+insert into orders values(9, 1, 2, 1, 100, 'test', '10086', 'earth', '100010', 'test', '10086', 'earth', '100010', 9, null);
+insert into orders values(10, 1, 2, 2, 100, 'test', '10086', 'earth', '100010', 'test', '10086', 'earth', '100010', 10, null);
+insert into orders values(11, 1, 2, 1, 100, 'test', '10086', 'earth', '100010', 'test', '10086', 'earth', '100010', 11, null);
