aa3911c496234fc5d3e9a62063c0363291945c01
glen <glen@hht-m>
2016-03-22 23:14:55 +0800

    run now. corn...

--- onepay/src/main/java/com/rainasmoon/onepay/service/job/SendEmailAccessLogJob.java
+++ onepay/src/main/java/com/rainasmoon/onepay/service/job/SendEmailAccessLogJob.java
@@ -1,17 +1,13 @@
 package com.rainasmoon.onepay.service.job;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 @Component
 public class SendEmailAccessLogJob {
-	public static Logger LOGGER = LoggerFactory
-			.getLogger(SendEmailAccessLogJob.class);
 
 	@Scheduled(cron = "0/5 * * * * ?")
 	public void run() {
-		LOGGER.info("MyFirstSpringJob trigger...");
+		System.out.print("fffffffffffffffffffffffffffffff");
 	}
 }
