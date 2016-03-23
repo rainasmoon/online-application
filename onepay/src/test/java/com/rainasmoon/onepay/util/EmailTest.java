f0f1b82a2a4c237238790196ea3735f2c3a17020
glen <glen@hht-m>
2016-03-23 21:37:53 +0800

    add des encrypt method.

--- onepay/src/test/java/com/rainasmoon/onepay/util/EmailTest.java
+++ onepay/src/test/java/com/rainasmoon/onepay/util/EmailTest.java
@@ -15,7 +15,8 @@ public class EmailTest {
 		Email email = new SimpleEmail();
 		email.setHostName("smtp.126.com");
 		email.setSmtpPort(25);
-		email.setAuthenticator(new DefaultAuthenticator("rainasmoon@126.com", "bsnpb6p*P"));
+		email.setAuthenticator(new DefaultAuthenticator("rainasmoon@126.com",
+				"bsnpb6p*P"));
 		email.setSSLOnConnect(false);
 		email.setFrom("rainasmoon@126.com");
 		email.setSubject("TestMail");
@@ -29,7 +30,7 @@ public class EmailTest {
 	public void shouldSendEmailWithAttachment() throws EmailException {
 		// Create the attachment
 		EmailAttachment attachment = new EmailAttachment();
-		attachment.setPath(EmailTest.class.getResource("sample.log").getFile());
+		attachment.setPath(EmailTest.class.getResource("test.log").getFile());
 		attachment.setDisposition(EmailAttachment.ATTACHMENT);
 		attachment.setDescription("Picture of John");
 		attachment.setName("John");
@@ -37,7 +38,8 @@ public class EmailTest {
 		// Create the email message
 		MultiPartEmail email = new MultiPartEmail();
 		email.setHostName("smtp.126.com");
-		email.setAuthenticator(new DefaultAuthenticator("rainasmoon@126.com", "bsnpb6p*P"));
+		email.setAuthenticator(new DefaultAuthenticator("rainasmoon@126.com",
+				"bsnpb6p*P"));
 		email.addTo("rainasmoon@126.com", "John Doe");
 		email.setFrom("rainasmoon@126.com", "Me");
 		email.setSubject("The picture");
