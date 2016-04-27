d711d2f5ed14b7186791f668cc3bc95d29f6f0cb
glen <rainasmoon@126.com>
2016-04-27 15:58:18 +0800

    get real ip from nginx

--- onepay/src/main/java/com/rainasmoon/onepay/util/AccessLogFilter.java
+++ onepay/src/main/java/com/rainasmoon/onepay/util/AccessLogFilter.java
@@ -1,6 +1,7 @@
 package com.rainasmoon.onepay.util;
 
 import java.io.IOException;
+import java.util.Enumeration;
 
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
@@ -11,6 +12,7 @@ import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpHeaders;
@@ -18,28 +20,23 @@ import org.springframework.http.server.ServletServerHttpRequest;
 
 public class AccessLogFilter implements Filter {
 
-	public static Logger LOGGER = LoggerFactory
-			.getLogger(AccessLogFilter.class);
+	public static Logger LOGGER = LoggerFactory.getLogger(AccessLogFilter.class);
 
 	@Override
 	public void destroy() {
 	}
 
 	@Override
-	public void doFilter(ServletRequest request, ServletResponse response,
-			FilterChain chain) throws IOException, ServletException {
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
 
 		HttpServletRequest httprequest = (HttpServletRequest) request;
 		String sessionId = httprequest.getRequestedSessionId();
 		String url = httprequest.getRequestURL().toString();
-		String userAgent = ((HttpServletRequest) request)
-				.getHeader("User-Agent");
-		String origin = ((HttpServletRequest) request)
-				.getHeader(HttpHeaders.ORIGIN);
+		String userAgent = ((HttpServletRequest) request).getHeader("User-Agent");
+		String origin = ((HttpServletRequest) request).getHeader(HttpHeaders.ORIGIN);
 
 		HttpSession session = ((HttpServletRequest) request).getSession();
-		Long loginUserId = (Long) session
-				.getAttribute(CommonConstants.LOGIN_USER_ID);
+		Long loginUserId = (Long) session.getAttribute(CommonConstants.LOGIN_USER_ID);
 
 		LOGGER.debug("-----------------------------------------------------------------------------");
 		LOGGER.debug("ACCESS:" + getIpAddr((HttpServletRequest) request));
@@ -53,8 +50,7 @@ public class AccessLogFilter implements Filter {
 		// check uri.getHost()
 
 		// LOGGER
-		ServletServerHttpRequest serverRequest = new ServletServerHttpRequest(
-				(HttpServletRequest) request);
+		ServletServerHttpRequest serverRequest = new ServletServerHttpRequest((HttpServletRequest) request);
 
 		LOGGER.debug("headers:" + serverRequest.getHeaders());
 
@@ -68,16 +64,33 @@ public class AccessLogFilter implements Filter {
 	}
 
 	public String getIpAddr(HttpServletRequest request) {
+		String ipFromNginx = request.getHeader("X-Real-IP");
 		String ip = request.getHeader("x-forwarded-for");
+
+		LOGGER.debug("X-Real-IP:" + ipFromNginx);
+		LOGGER.debug("x-forwarded-for:" + ip);
+
+		if (StringUtils.isNotBlank(ipFromNginx)) {
+			return ipFromNginx;
+		}
+
 		if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
 			ip = request.getHeader("Proxy-Client-IP");
+			LOGGER.debug("Proxy-Client-IP:" + ip);
 		}
 		if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
 			ip = request.getHeader("WL-Proxy-Client-IP");
+			LOGGER.debug("WL-Proxy-Client-IP:" + ip);
 		}
 		if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
 			ip = request.getRemoteAddr();
+			LOGGER.debug("getRemoteAddr:" + ip);
 		}
+		Enumeration<String> headerNames = request.getHeaderNames();
+		while (headerNames.hasMoreElements()) {
+			LOGGER.debug("headers name:" + headerNames.nextElement());
+		}
+
 		return ip;
 	}
 }
