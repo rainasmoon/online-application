91e316ba216ad9dfd94df09f459e957db3927af9
glen <rainasmoon@126.com>
2016-05-18 15:23:25 +0800

    add msg service

---
+++ onepay/src/test/java/com/rainasmoon/onepay/util/MessageTest.java
@@ -0,0 +1,30 @@
+package com.rainasmoon.onepay.util;
+
+import org.junit.Test;
+
+import com.taobao.api.ApiException;
+import com.taobao.api.DefaultTaobaoClient;
+import com.taobao.api.TaobaoClient;
+import com.taobao.api.request.AlibabaAliqinFcSmsNumSendRequest;
+import com.taobao.api.response.AlibabaAliqinFcSmsNumSendResponse;
+
+public class MessageTest {
+
+	private String url = "http://gw.api.taobao.com/router/rest";
+	private String appkey = "23368055";
+	private String secret = "2e6150177b3c7894bce257b630b82561";
+
+	@Test
+	public void test() throws ApiException {
+		TaobaoClient client = new DefaultTaobaoClient(url, appkey, secret);
+		AlibabaAliqinFcSmsNumSendRequest req = new AlibabaAliqinFcSmsNumSendRequest();
+		req.setExtend("123456");
+		req.setSmsType("normal");
+		req.setSmsFreeSignName("阿里大鱼");
+		req.setSmsParamString("{\"code\":\"1234\",\"product\":\"alidayu\"}");
+		req.setRecNum("13000000000");
+		req.setSmsTemplateCode("SMS_585014");
+		AlibabaAliqinFcSmsNumSendResponse rsp = client.execute(req);
+		System.out.println(rsp.getBody());
+	}
+}
