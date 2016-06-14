804bce730bac0fcad5e62c3696127f1f75539fdd
glen <rainasmoon@126.com>
2016-06-14 15:59:40 +0800

    1, add goaccessrc & create how to create html.

--- onepay/script/download_aws_config.bat
+++ onepay/script/download_aws_config.bat
@@ -4,5 +4,6 @@ scp -i "/cygdrive/c/redhat.pem"  ec2-user@aws.rainasmoon.com:/etc/fstab ./aws/fs
 scp -i "/cygdrive/c/redhat.pem"  ec2-user@aws.rainasmoon.com:~/autodeploy.sh ./aws/autodeploy.sh
 scp -i "/cygdrive/c/redhat.pem"  ec2-user@aws.rainasmoon.com:~/backupdb.sh ./aws/backupdb.sh
 scp -i "/cygdrive/c/redhat.pem"  ec2-user@aws.rainasmoon.com:~/report.sh ./aws/report.sh
+scp -i "/cygdrive/c/redhat.pem"  ec2-user@aws.rainasmoon.com:~/.goaccessrc ./aws/.goaccessrc
 scp -i "/cygdrive/c/redhat.pem"  ec2-user@aws.rainasmoon.com:/mnt/b/backup/* ./aws/backup/
 pause
