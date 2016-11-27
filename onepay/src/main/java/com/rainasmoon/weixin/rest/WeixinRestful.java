42fdbe8acefa7cf344517f39970175a879252c2a
hht <hht@98.124.199.54>
2016-11-27 13:17:23 +0800

    test event

--- onepay/src/main/java/com/rainasmoon/weixin/rest/WeixinRestful.java
+++ onepay/src/main/java/com/rainasmoon/weixin/rest/WeixinRestful.java
@@ -43,24 +43,48 @@ public class WeixinRestful {
 		String accessToken = WeixinCommonConstants.getAccessToken();
 		String msgType = requestJson.getString("MsgType");
 		LOGGER.debug("MsgType is :" + msgType);
+		if ("event".equals(msgType)) {
 
-		String msgid = requestJson.getString("MsgId");
-		String content = requestJson.getString("Content");
-		String fromUserName = requestJson.getString("FromUserName");
-		String toUserName = requestJson.getString("ToUserName");
-		String createTime = requestJson.getString("CreateTime");
+			String FromUserName = requestJson.getString("FromUserName");
+			Long CreateTime = requestJson.getLong("CreateTime");
 
-		String replayContent = "<a href=\"http://www.rainasmoon.com/\">到一元网看看吧</a>";
+			String event = requestJson.getString("Event");
+			LOGGER.debug("Event: " + event);
+			// 关注
+			if (event.equals("subscribe")) {
 
-		return XMLUtil.createXML(createResponseJson(fromUserName, toUserName, replayContent));
+			} else if (event.equals("unsubscribe")) {
 
+			} else if (event.equals("VIEW")) {
+
+				// 点击事件
+			} else if (event.equals("CLICK")) {
+				String eventKey = requestJson.getString("EventKey");
+
+			} else {
+				LOGGER.warn("cant deal with this weixin event: " + event);
+			}
+
+		} else if ("text".equals(msgType)) {
+			String msgid = requestJson.getString("MsgId");
+			String content = requestJson.getString("Content");
+			String fromUserName = requestJson.getString("FromUserName");
+			String toUserName = requestJson.getString("ToUserName");
+			String createTime = requestJson.getString("CreateTime");
+
+			String replayContent = "<a href=\"http://www.rainasmoon.com/\">到一元网看看吧</a>";
+
+			return XMLUtil.createXML(createResponseJson(fromUserName, toUserName, replayContent));
+		}
+
+		return "";
 	}
 
 	private JSONObject createResponseJson(String fromUserName, String toUserName, String content) {
 		JSONObject resultJson = new JSONObject();
 		resultJson.put("ToUserName", fromUserName);
 		resultJson.put("FromUserName", toUserName);
-		resultJson.put("CreateTime", new Date().toString());
+		resultJson.put("CreateTime", new Date().getTime());
 		resultJson.put("MsgType", "text");
 		resultJson.put("Content", content);
 
