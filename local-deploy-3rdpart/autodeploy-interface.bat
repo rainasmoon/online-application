db29c92729e60e4619dace452ba8bf090602b0c0
glen <rainasmoon@126.com>
2017-06-05 17:36:55 +0800

    remove unused code

--- local-deploy-3rdpart/autodeploy-interface.bat
+++
@@ -1,7 +0,0 @@
-set JAVA_HOME=D:/jdk1.8.0_66
-set MAVEN_HOME=D:/apache-maven-3.0.3
-export PATH=.:/cygdrive/d/apache-maven-3.0.3/bin:`printenv PATH`
-cd D:/workspace/platform/core/trunk/mc/mc-user-interface
-call mvn clean deploy -f pom-3rd.xml
-cd D:/workspace/platform/util/trunk/utility/utility-exception
-call mvn clean deploy -f pom-3rd.xml
--- local-deploy-3rdpart/src/com/wanlianjin/test/Test.java
+++
@@ -1,8 +0,0 @@
-package com.wanlianjin.test;
-
-public class Test {
-	public static void main(String[] arg) {
-		String md5 = "1234567890abcd";
-		System.out.println(String.format("%s%s", md5.substring(10), md5.substring(0, 10)));
-	}
-}
