275e3c7768cca248181e13ddb1a7ac78b0984932
glen <rainasmoon@126.com>
2017-06-06 18:18:29 +0800

    fix channel

--- onepay/src/test/java/com/rainasmoon/netty/DiscardServerHandler.java
+++ onepay/src/test/java/com/rainasmoon/netty/DiscardServerHandler.java
@@ -7,9 +7,9 @@ import io.netty.buffer.ByteBuf;
 import io.netty.channel.ChannelFuture;
 import io.netty.channel.ChannelFutureListener;
 import io.netty.channel.ChannelHandlerContext;
-import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.channel.SimpleChannelInboundHandler;
 
-public class DiscardServerHandler extends ChannelInboundHandlerAdapter { // (1)
+public class DiscardServerHandler extends SimpleChannelInboundHandler<Object> { // (1)
 
     Logger LOGGER = LoggerFactory.getLogger(DiscardServerHandler.class);
 
@@ -34,7 +34,7 @@ public class DiscardServerHandler extends ChannelInboundHandlerAdapter { // (1)
     @Override
     public void channelRead(ChannelHandlerContext ctx, Object msg) { // (2)
 
-
+        LOGGER.info("channel read...");
         ctx.write(msg); // (1)
         ctx.flush(); // (2)
 
@@ -46,5 +46,11 @@ public class DiscardServerHandler extends ChannelInboundHandlerAdapter { // (1)
         // Close the connection when an exception is raised.
         cause.printStackTrace();
         ctx.close();
+    }
+
+    @Override
+    protected void channelRead0(ChannelHandlerContext ctx, Object msg) throws Exception {
+        LOGGER.info("channel read0...");
+
     }
 }
--- onepay/src/test/java/com/rainasmoon/netty/TimeClient.java
+++ onepay/src/test/java/com/rainasmoon/netty/TimeClient.java
@@ -1,6 +1,10 @@
 package com.rainasmoon.netty;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.Channel;
 import io.netty.channel.ChannelFuture;
 import io.netty.channel.ChannelInitializer;
 import io.netty.channel.ChannelOption;
@@ -8,6 +12,8 @@ import io.netty.channel.EventLoopGroup;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.handler.codec.http.websocketx.TextWebSocketFrame;
+import io.netty.handler.codec.http.websocketx.WebSocketFrame;
 
 public class TimeClient {
     public static void main(String[] args) throws Exception {
@@ -30,8 +36,22 @@ public class TimeClient {
             // Start the client.
             ChannelFuture f = b.connect(host, port).sync(); // (5)
 
+            Channel ch = f.channel();
+
             // Wait until the connection is closed.
             f.channel().closeFuture().sync();
+
+            BufferedReader console = new BufferedReader(new InputStreamReader(System.in, "UTF-8"));
+            while (true) {
+                String msg = console.readLine();
+                if (msg == null) {
+                    break;
+                }
+
+                WebSocketFrame frame = new TextWebSocketFrame(msg);
+                ch.writeAndFlush(frame);
+            }
+
         } finally {
             workerGroup.shutdownGracefully();
         }
