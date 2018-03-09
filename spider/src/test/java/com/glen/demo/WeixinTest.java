1ac5305703b58acfe0e3338c3e459c19f3274088
glen <rainasmoon@126.com>
2018-03-09 14:01:13 +0800

    weixin appium worked

--- spider/src/test/java/com/glen/demo/WeixinTest.java
+++ spider/src/test/java/com/glen/demo/WeixinTest.java
@@ -1,5 +1,6 @@
 package com.glen.demo;
 
+import java.io.File;
 import java.net.URL;
 import java.util.List;
 
@@ -8,6 +9,7 @@ import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.CapabilityType;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.slf4j.Logger;
@@ -20,14 +22,14 @@ public class WeixinTest {
 
     private static Logger log = LoggerFactory.getLogger("TTT");
 
-    private AppiumDriver<WebElement> driver;
+    private AppiumDriver driver;
 
     @Before
     public void setUp() throws Exception {
 
         DesiredCapabilities capabilities = getRealPhone();
         // 初始化
-        driver = new AndroidDriver<WebElement>(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
+        driver = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
 
     }
 
@@ -54,61 +56,63 @@ public class WeixinTest {
         capabilities.setCapability("unicodeKeyboard", "True");
         capabilities.setCapability("resetKeyBoard", "True");
 
-//        ChromeOptions options = new ChromeOptions();
-//        options.setExperimentalOption("androidProcess", "com.tencent.mm:appbrand1");
+        ChromeOptions options = new ChromeOptions();
+        options.setExperimentalOption("androidProcess", "com.tencent.mm:appbrand1");
 
-//        capabilities.setCapability(ChromeOptions.CAPABILITY, options);
+        capabilities.setCapability(ChromeOptions.CAPABILITY, options);
 
         return capabilities;
     }
 
     // adb connect 127.0.0.1:62001
 
-//    private DesiredCapabilities getPhoneAVD() {
-//
-//        File classpathRoot = new File(System.getProperty("user.dir"));
-//        File appDir = new File(classpathRoot, "apps");
-//        File app = new File(appDir, "weixin.apk");
-//
-//        // 设置自动化相关参数
-//        DesiredCapabilities capabilities = new DesiredCapabilities();
-//        capabilities.setCapability(CapabilityType.BROWSER_NAME, "");
-//        capabilities.setCapability("platformName", "Android");
-//        capabilities.setCapability("deviceName", "test");
-//        // capabilities.setCapability("deviceName", "127.0.0.1:62001");
-//        // capabilities.setCapability("udid", "127.0.0.1:62001");
-//
-//        // 设置安卓系统版本
-//        capabilities.setCapability("platformVersion", "4.3");
-//        capabilities.setCapability("fastReset", "false");
-//        capabilities.setCapability("fullReset", "false");
-//        capabilities.setCapability("noReset", "true");
-//        capabilities.setCapability("newCommandTimeout", 24000);
-//        capabilities.setCapability("device", "appium");
-//
-//        // 设置app的主包名和主类名
-//        capabilities.setCapability("app", app.getAbsolutePath());
-//
-//        capabilities.setCapability("appPackage", "com.tencent.mm");
-//        capabilities.setCapability("appActivity", ".ui.LauncherUI");
-//
-//        return capabilities;
-//    }
+    private DesiredCapabilities getPhoneAVD() {
+
+        File classpathRoot = new File(System.getProperty("user.dir"));
+        File appDir = new File(classpathRoot, "apps");
+        File app = new File(appDir, "weixin.apk");
+
+        // 设置自动化相关参数
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability(CapabilityType.BROWSER_NAME, "");
+        capabilities.setCapability("platformName", "Android");
+        capabilities.setCapability("deviceName", "test");
+        // capabilities.setCapability("deviceName", "127.0.0.1:62001");
+        // capabilities.setCapability("udid", "127.0.0.1:62001");
+
+        // 设置安卓系统版本
+        capabilities.setCapability("platformVersion", "4.3");
+        capabilities.setCapability("fastReset", "false");
+        capabilities.setCapability("fullReset", "false");
+        capabilities.setCapability("noReset", "true");
+        capabilities.setCapability("newCommandTimeout", 24000);
+        capabilities.setCapability("device", "appium");
+
+        // 设置app的主包名和主类名
+        capabilities.setCapability("app", app.getAbsolutePath());
+
+        capabilities.setCapability("appPackage", "com.tencent.mm");
+        capabilities.setCapability("appActivity", ".ui.LauncherUI");
+
+        return capabilities;
+    }
 
     @Test
     public void openWebWeixin() throws InterruptedException {
 
-        loginWeixin();
-                
-//        driver.findElementByXPath("//*[@text='发现']").click();
-//        driver.findElementByXPath("//*[@text='小程序']").click();
-//        driver.findElementByXPath("//*[contains(@text, '美团外卖')]").click();
-//        println(driver.getContextHandles());
+//        loginWeixin();
+        
+        
+        driver.findElementByXPath("//*[@text='泰康在线保险']").click();
+        driver.findElementByXPath("//*[@text='保险商城']").click();
+        
+
+        println(driver.getContextHandles());
 //        driver.context("WEBVIEW_com.tencent.mm:tools");
-//        Thread.sleep(5000);
-//        println(driver.getPageSource());
-//        driver.findElementByXPath("//*[contains(@url, '美食')]").click();
-//        driver.findElementByXPath("//*[contains(., '金百万')]").click();
+        Thread.sleep(5000);
+        println(driver.getPageSource());
+//        driver.findElementByXPath("//*[contains(@url, '全部')]").click();
+//        driver.findElementByXPath("//*[contains(., '全部')]").click();
     }
 
     private void loginWeixin() {
