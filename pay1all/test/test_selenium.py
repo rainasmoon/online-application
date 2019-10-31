367be32f1c8e33d881710179180a38d862c4b93a
Administrator <Administrator@WIN-FADKF8SMQOB.lan>
2019-10-31 14:12:15 +0800

    add selenium and test pass

--- pay1all/test/test_selenium.py
+++ pay1all/test/test_selenium.py
@@ -8,10 +8,10 @@ from selenium.webdriver.support.wait import WebDriverWait
 if __name__ == "__main__":
     options = Options()
     options.add_argument('-headless')  # 无头参数
-    driver = Firefox(executable_path='geckodriver', options=options)  # 配了环境变量第一个参数就可以省了，不然传绝对路径
+    driver = Firefox(executable_path='C:\\Users\\Administrator\\git\\online-application\\pay1all\\lib\\geckodriver.exe', options=options)  # 配了环境变量第一个参数就可以省了，不然传绝对路径
     wait = WebDriverWait(driver, timeout=10)
-    driver.get('http://www.google.com')
-    wait.until(expected.visibility_of_element_located((By.NAME, 'q'))).send_keys('headless firefox' + Keys.ENTER)
-    wait.until(expected.visibility_of_element_located((By.CSS_SELECTOR, '#ires a'))).click()
+    driver.get('https://www.google.com')
+#     wait.until(expected.visibility_of_element_located((By.NAME, 'q'))).send_keys('headless firefox' + Keys.ENTER)
+#     wait.until(expected.visibility_of_element_located((By.CSS_SELECTOR, '#ires a'))).click()
     print(driver.page_source)
     driver.quit()
