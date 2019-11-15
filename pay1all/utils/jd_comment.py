733a0d9f23beb84bde267145a25da2a728dc574c
Administrator <Administrator@WIN-FADKF8SMQOB.lan>
2019-11-15 14:15:42 +0800

    test and fine wordcloud stopword

--- pay1all/utils/jd_comment.py
+++ pay1all/utils/jd_comment.py
@@ -9,7 +9,7 @@ import jieba
 import requests
 
 import numpy as np
-from wordcloud import WordCloud
+from wordcloud import WordCloud, STOPWORDS
 
 STATIC_PIC_PATH = '/var/www/html/static/cloudpic/'
 COMMENT_FILE_PATH = STATIC_PIC_PATH + 'jd_comment.txt'
@@ -18,8 +18,7 @@ WC_FONT_PATH = '/usr/share/fonts/truetype/wqy/wqy-zenhei.ttc'
 
 MAX_PAGE_NUM = 10
 
-test_jd_prod_sku_1 = 25643981948
-test_jd_prod_sku_2 = 1263013576
+test_jd_prod_sku_1 = 1263013576
 
 
 def spider_comment(sku_id, page=0):
@@ -98,7 +97,9 @@ def create_word_cloud(sku_id):
     if not os.path.exists(r_file_path):
         print('MAKE CLOUD PIC...:', r_file_path)
         # 设置词云的一些配置，如：字体，背景色，词云形状，大小
-        wc = WordCloud(background_color="white", max_words=2000, scale=4,
+        stopwords = set(STOPWORDS)
+        stopwords.add("said")
+        wc = WordCloud(background_color="white", max_words=2000, stopwords=stopwords, scale=4, width=1000, height=860, margin=2,
                        max_font_size=50, random_state=42, font_path=WC_FONT_PATH)
         # 生成词云
         r_cutword = cut_word(sku_id)
