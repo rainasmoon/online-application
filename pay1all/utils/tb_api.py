8716bcd2de265d8ae08e892f6a1005cb55959a6c
rain <rainasmoon@126.com>
2019-12-13 18:15:49 +0800

    init taobao api

--- pay1all/utils/tb_api.py
+++ pay1all/utils/tb_api.py
@@ -1,83 +1,46 @@
-# -*- coding: UTF-8 -*- 
-'''
-Created on 2019-10-23
-
-@author: Administrator
-'''
-
-import datetime
-import hashlib
-from io import StringIO
+# -*- coding: utf-8 -*-
+import top.api
 import json
-import time
-from urllib import parse
-from urllib import request
 
 f = open('config_api.json', 'r')
 config_jd_api = json.load(f)
-app_key = config_jd_api['tb_app_key']
-appsecret = config_jd_api['tb_appsecret']
-site_id = '1728182420'
-position_id = '1915772300'
-access_token = ''
+appkey = config_jd_api['tb_app_key']
+secret = config_jd_api['tb_appsecret']
 
-method_get_promotion = 'jd.union.open.promotion.common.get'
+url = 'gw.api.taobao.com'
+port = '80'
 
 
-STATUS_OK = 200
-
-
-def get_material_id(jd_prod_sku):
-    return 'https://item.jd.com/' + str(jd_prod_sku) + '.html'
-
-
-def get_timestamp():
-    return time.strftime('%Y-%m-%d %H:%M:%S', time.localtime())
-
-def get_param_json_kouling(atime, page_no):
-    return '{"orderReq":{"time":"' + atime + '","pageNo":' + str(page_no) + ',"pageSize":"500","type":"1"}}'
-
-
-
-
-def get_sign_key(method, timestamp, param_json):
-    
-    c = ''
-    # c = c + 'access_token' + access_token
-    c = c + 'app_key' + app_key
-    c = c + 'formatjson'
-    c = c + 'method' + method
-    c = c + 'param_json' + param_json
-    c = c + 'sign_methodmd5'
-    c = c + 'timestamp' + timestamp
-    c = c + 'v1.0'
-    c = appsecret + c + appsecret
-    sign_key = hashlib.md5(c.encode("utf-8")).hexdigest().upper()
-    return sign_key
-    
-
-def get_api_url(method, param_json):
-    timestamp = get_timestamp()
-    
-    sign_key = get_sign_key(method, timestamp, param_json)
-    return 'http://gw.api.taobao.com/router/rest'
-
-
-def call_tb_api(api_url):
-    print('CALL TB API:' + api_url)
-    rresponse = request.urlopen(api_url)
-    s_result = rresponse.read()
-    print('TB RESPONSES:' + str(s_result))
-    json_result = json.loads(s_result)  
-    return json_result
-
-    
 def call_tb_kouling():
-    param_json = get_param_json_category(paraent_id, agrade)
-    json_result = call_tb_api(get_api_url(method_get_category, param_json))
-    return json_result
+    req=top.api.TbkTpwdCreateRequest(url, port)
+    req.set_app_info(top.appinfo(appkey,secret))
 
+    req.user_id="108721660"
+    req.text="长度大于5个字符"
+    req.url="https://uland.taobao.com/"
+    req.logo="https://uland.taobao.com/"
+    req.ext="{}"
+    try:
+        resp= req.getResponse()
+        print(resp)
+    except Exception as e:
+        print(e)
+
+
+def call_tb_search():
+    req=top.api.TbkDgMaterialOptionalRequest(url,port)
+    req.set_app_info(top.appinfo(appkey,secret))
+
+    req.adzone_id=109847100359
+    req.q='洛天依cos'
+
+    try:
+        resp= req.getResponse()
+        print(resp)
+    except Exception as e:
+        print(e)
 
 
 if __name__ == '__main__':
-    call_tb_kouling();
+    #call_tb_kouling()
+    call_tb_search()
